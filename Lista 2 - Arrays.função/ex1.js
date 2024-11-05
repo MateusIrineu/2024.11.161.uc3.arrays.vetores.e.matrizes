14d7d43704422d3f88499ff74b4661e7bdd2a080
mateus irineu <irineu.mateus11@gmail.com>
2024-11-05 19:00:21 -0300

    a

--- Lista 2 - Arrays.função/ex1.js
+++ Lista 2 - Arrays.função/ex1.js
@@ -1,9 +1,12 @@
-/* Enunciado: Crie um array chamado fruits com os elementos "apple", "banana", "orange". Use o método .push() para adicionar "grape" ao final do array e o método .pop() para remover o último elemento. 
+/* Enunciado: Crie um array chamado fruits com os elementos "apple", "banana", "orange". Use o método .push() para adicionar "grape" ao final do array 
+e o método .pop() para remover o último elemento. 
 Qual será o conteúdo do array após essas operações? */
 /* Entrada: ["apple", "banana", "orange"]  */
 
 let frutas = ["apple", "banana", "oragen"];
 
 frutas.push("grape")
+
+frutas.pop()
 
 console.log(frutas)
